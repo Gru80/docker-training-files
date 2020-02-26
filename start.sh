bd5cae70641cde266d0cb4143f45d9eade184bd6
Daniel Hoeggerl <daniel.hoeggerl@trigo.at>
2020-02-26 10:32:56 +0100

    add startup script

---
+++ start.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+docker network create demo_internal
+docker network create demo_dmz
+
+volume=$(docker volume create --label app=demo --label service=redis)
+
+docker run -d --net=demo_internal --name redis \
+	-v ${volume}:/data \
+	redis:3.2 redis-server --appendonly yes
+
+docker run -d --net=demo_internal --name request-counter \
+	-e REDIS_HOST=redis.demo_internal \
+	-p 8090:8090 \
+	mdulghier/request-counter
+
+docker network connect demo_dmz request-counter
+	
+docker run -d --net=demo_dmz --name hello-world \
+	-p 8080:8080 \
+	-e REQUESTCOUNTERURL=http://request-counter.demo_dmz:8090 \
+	mdulghier/hello-world
+
+
+
